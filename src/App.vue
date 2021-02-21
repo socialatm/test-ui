46af02f3044537f7a73d627f6ace4e51f8afe5bc
Ray <raypeaslee@gmail.com>
2021-02-21 11:06:51 -0500

    Update App.vue
    just some styling

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@
     </router-link> |
     <router-link to="/about">
       About
-    </router-link>
+    </router-link> |
     <router-link to="/register">
       Register
-    </router-link>
+    </router-link> |
     <router-link to="/login">
       Login
     </router-link>
