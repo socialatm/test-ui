e60c36826ae35df4ca4629a8fce9071fd0fcd975
Ray <raypeaslee@gmail.com>
2021-03-01 11:40:15 -0500

    Update main.js
    uninstall fontawesome and bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -6,7 +6,5 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import './plugins/fontawesome'
-import './plugins/bootstrap-vue'
 
 createApp(App).use(store).use(router).use(VueAxios, axios).mount('#app')
